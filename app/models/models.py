5fcf507eac2d9b6be0908eb9838eb3436066d90d
Kevin Koech <kevinkoech@proton.me>
2023-09-13 23:19:16 +0300

    Add models dir

---
+++ app/models/models.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from flask_sqlalchemy import Column, Integer, String
+from flask_marshmallow import Marshmallow
+from .app.databse.db import db
+
+app = Marshmallow()
+
+class Person(db.Model):
+    '''
+    This class contains name, which is a string,
+    resource of Person.
+    '''
+    __tablename__ = 'person'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name):
+        '''
+        This method initializes name attribute
+        of Person object.
+        '''
+        self.name = name
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def __str__(self):
+        return self.name
+
+
+class PersonSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        fields = ('id', 'name')
+
+person_schema = PersonSchema()
+people_schema = PersonSchema(many=True)
+
