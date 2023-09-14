b766f890eb57dbabbca7ca390f177270017d016a
Kevin Koech <kevinkoech@proton.me>
2023-09-14 11:03:26 +0300

    reorder importations

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, jsonify
+from flask import Flask
 from app.database.db import db
-from app.models import *
-from app.routes import *
 
 DB_NAME = 'test.db'
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "notasecret iguess"
-# configure database location, will be in app directory
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-# initiliaze database
-db.init_app(app)
+def start_app():
+
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "notasecret iguess"
+    # configure database location, will be in app directory
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # initiliaze database
+    db.init_app(app)
+
+    from app.models.models import Person, PersonSchema
+    from app.routes.person_routes import person_routes
+
+    app.register_blueprint(person_routes)
+
+    create_database(app)
+
+    return app
 
 def create_database(app):
     # Create database with all tables specified
     with app.app_context():
         db.create_all()
     print('Database created successfully!')
-
-if __name__ == '__main__':
-    create_database(app)
-
-    app.run(debug=True)
-
 
