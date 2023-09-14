46cdd7cf5809a7ac51ebd07171e6d68de015afad
Kevin Koech <kevinkoech@proton.me>
2023-09-14 11:04:06 +0300

    use Blueprint to create routes

--- app/routes/person_routes.py
+++ app/routes/person_routes.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
 
-from flask import request, jsonify
+from flask import Blueprint, request, jsonify
 from app.database.db import db
+from app.models.models import *
 
-@app.route('/api', methods=['POST'])
+person_routes = Blueprint('api', __name__)
+
+@person_routes.route('/api', methods=['POST'])
 def create_person():
     name = request.json['name']
-    age = request.json.get('age')
     new_person = Person(name=name)
     db.session.add(new_person)
     db.session.commit()
     return person_schema.jsonify(new_person)
 
-@app.route('/api/<int:person_id>', methods=['GET'])
+@person_routes.route('/api/<int:person_id>', methods=['GET'])
 def get_person(person_id):
     person = Person.query.get(person_id)
     return person_schema.jsonify(person)
 
-@app.route('/api/<int:person_id>', methods=['PUT'])
+@person_routes.route('/api/<int:person_id>', methods=['PUT'])
 def update_person(person_id):
     person = Person.query.get(person_id)
     person.name = request.json['name']
     db.session.commit()
     return person_schema.jsonify(person)
 
-@app.route('/api/<int:person_id>', methods=['DELETE'])
+@person_routes.route('/api/<int:person_id>', methods=['DELETE'])
 def delete_person(person_id):
     person = Person.query.get(person_id)
     db.session.delete(person)
     db.session.commit()
     return jsonify({'message': 'Person deleted successfully'})
-
 
